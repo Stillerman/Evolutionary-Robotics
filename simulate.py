9727b44e26c77d676a6e703a56b861aea5337012
Jason Stillerman <jason.t.stillerman@gmail.com>
2022-02-07 15:15:12 -0500

    patched pyrosim

--- simulate.py
+++ simulate.py
@@ -21,6 +21,14 @@ for i in range(1000):
 	p.stepSimulation()
 	backLegHist[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegHist[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+	pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robotId,
+		jointName = "Torso_BackLeg",
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = 0.0,
+		maxForce = 500 # 500 NM
+	)
 	time.sleep(1/600)
 
 np.save("data/backLegSensorValues.npy", backLegHist)
